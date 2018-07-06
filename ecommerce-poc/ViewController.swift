f50d3ceb364a54f9a29884f2d36eb7e6058889d3
Rugmangathan <mkrugmangathan@gmail.com>
2018-07-06 12:36:21 +0530

    The saga begins!

---
+++ ecommerce-poc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ecommerce-poc
+//
+//  Created by Rugmangathan on 06/07/18.
+//  Copyright © 2018 Rugmangathan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
