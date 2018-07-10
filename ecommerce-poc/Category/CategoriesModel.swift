3449432c0b6d506f50c70317e27f519771d6ba28
Rugmangathan <mkrugmangathan@gmail.com>
2018-07-10 11:43:03 +0530

    CategoriesModel, updated restored state.

--- ecommerce-poc/Category/CategoriesModel.swift
+++ ecommerce-poc/Category/CategoriesModel.swift
@@ -14,7 +14,7 @@ class CategoriesModel {
                   _ cachedRepository: CachedRepository,
                   _ states: Observable<CategoriesState>) -> Observable<CategoriesState> {
     let initialState = CategoriesState()
-
+    
     let lifecycleStates = lifecycle
       .filter { $0 == .created }
       .flatMapLatest { _ in
@@ -23,27 +23,27 @@ class CategoriesModel {
             let copiedState = CategoriesState(initialState)
             copiedState.fetchAction = fetchEvent.fetchAction
             copiedState.categories = fetchEvent.result ?? []
-
+            
             return copiedState
         }
     }
-
+    
     let restoredStates = lifecycle
       .filter { $0 == .restored }
       .withLatestFrom(states) { (_, state: CategoriesState) -> CategoriesState in
         return state
       }
-      .flatMapLatest { _ in
+      .flatMapLatest { state in
         return cachedRepository.getCategories()
           .flatMapLatest { (fetchEvent) -> Observable<CategoriesState> in
-            let copiedState = CategoriesState(initialState)
+            let copiedState = CategoriesState(state)
             copiedState.fetchAction = fetchEvent.fetchAction
             copiedState.categories = fetchEvent.result ?? []
-
+            
             return Observable.just(copiedState)
         }
     }
-
+    
     return Observable.merge(lifecycleStates, restoredStates)
   }
 }
